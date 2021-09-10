df43c20c954d54fede08aeae05a2c71e5df783ca
AlphaYan <battlelab@qq.com>
2021-09-10 10:37:03 +0800

    Updates

---
+++ component---src-pages-404-js-07f4b72207745b12172f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_2bo_blog=self.webpackChunk_2bo_blog||[]).push([[883],{9616:function(t,e,a){a.r(e);var n=a(7294),l=a(7198),o=a(3751);e.default=function(t){var e=t.data,a=t.location,u=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:u},n.createElement(o.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-07f4b72207745b12172f.js.map
